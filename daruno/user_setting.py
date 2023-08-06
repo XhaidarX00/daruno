6667b3defff2c24d6415c26440db87bca1cc827b
XhaidarX00 <miskandar772000@gmail.com>
2023-08-06 22:33:53 +0700

    fix

--- daruno/user_setting.py
+++ daruno/user_setting.py
@@ -24,7 +24,8 @@ from .database import db
 
 class UserSetting(db.Entity):
 
-    id = PrimaryKey(int, auto=False, size=64)  # Telegram User ID
+    # id = PrimaryKey(int, auto=False, size=64)  # Telegram User ID
+    id = PrimaryKey(int, size=64)  # Telegram User ID
     lang = Optional(str, default='')  # The language setting for this user
     stats = Optional(bool, default=False)  # Opt-in to keep game statistics
     first_places = Optional(int, default=0)  # Nr. of games won in first place
